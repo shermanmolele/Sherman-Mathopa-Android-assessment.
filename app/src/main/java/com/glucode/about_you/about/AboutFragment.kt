71a6d82263b09efe58d899f41f8b18c0c27d8400
Sherman Mathopa <shermanmolele@gmail.com>
2024-06-16 14:22:21 +0200

    Select image to upload

--- app/src/main/java/com/glucode/about_you/about/AboutFragment.kt
+++ app/src/main/java/com/glucode/about_you/about/AboutFragment.kt
@@ -1,17 +1,52 @@
 package com.glucode.about_you.about
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.glucode.about_you.about.views.ProfileCardView
 import com.glucode.about_you.about.views.QuestionCardView
 import com.glucode.about_you.databinding.FragmentAboutBinding
+import com.glucode.about_you.engineers.models.Engineer
 import com.glucode.about_you.mockdata.MockData
+import kotlinx.coroutines.flow.collect
 
 class AboutFragment: Fragment() {
     private lateinit var binding: FragmentAboutBinding
+    private val viewModel: AboutViewModel by viewModels()
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                openGallery()
+            } else {
+               Toast.makeText(context, "Please allow storage permission to upload image", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val pickImage =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val imageUri = result.data?.data
+                imageUri?.let { uri ->
+                    viewModel.updateProfileImage(uri)
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,21 +60,42 @@ class AboutFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupEngineerProfile()
-        setUpQuestions()
-    }
-
-    private fun setupEngineerProfile() {
         val engineerName = arguments?.getString("name")
         val engineer = MockData.engineers.first { it.name == engineerName }
 
+        setupEngineerProfile(engineer)
+        viewModel.updateEngineerName(engineer.name)
+        setUpQuestions()
+    }
 
-        val profileCardView = ProfileCardView(requireContext())
-        profileCardView.name = engineer.name
-        profileCardView.role = engineer.role
-        profileCardView.years = engineer.quickStats.years
-        profileCardView.bugs = engineer.quickStats.bugs
-        profileCardView.coffees = engineer.quickStats.coffees
+    private fun setupEngineerProfile(engineer: Engineer) {
+
+        val profileCardView = ProfileCardView(requireContext()) {
+            if (ContextCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermission()
+            } else {
+                openGallery()
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.imageUri.collect {
+                profileCardView.imageUri = it
+            }
+        }
+        with(engineer) {
+
+            if (defaultImageName != "")
+             profileCardView.imageUri = defaultImageName.toUri()
+            profileCardView.name = engineer.name
+            profileCardView.role = engineer.role
+            profileCardView.years = engineer.quickStats.years
+            profileCardView.bugs = engineer.quickStats.bugs
+            profileCardView.coffees = engineer.quickStats.coffees
+        }
 
         binding.container.addView(profileCardView)
     }
@@ -56,5 +112,28 @@ class AboutFragment: Fragment() {
 
             binding.container.addView(questionView)
         }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        pickImage.launch(galleryIntent)
+    }
+
+    private fun requestPermission() {
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+            else -> {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_EXTERNAL_STORAGE_PERMISSION
+                )
+            }
+        }
+    }
+    companion object {
+        private const val REQUEST_EXTERNAL_STORAGE_PERMISSION = 1
     }
 }
---
+++ app/src/main/java/com/glucode/about_you/about/AboutViewModel.kt
@@ -0,0 +1,33 @@
+package com.glucode.about_you.about
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import com.glucode.about_you.mockdata.MockData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class AboutViewModel: ViewModel() {
+    private var _imageUri = MutableStateFlow<Uri?>(null)
+    val imageUri = _imageUri.asStateFlow()
+
+
+    private var _engineerName = MutableStateFlow("")
+
+    fun updateProfileImage(uri: Uri) {
+        _imageUri.value = uri
+        val engineer = MockData.engineers.first { it.name == _engineerName.value }
+        val updateEngineerImage = engineer.copy(defaultImageName = uri.toString())
+
+        val index = MockData.engineers.indexOfFirst { it.name == _engineerName.value }
+
+        if (index != -1) {
+            MockData.engineers[index] = updateEngineerImage
+        } else {
+            println("Engineer ${_engineerName.value} not found.")
+        }
+    }
+
+    fun updateEngineerName(name: String) {
+        _engineerName.value = name
+    }
+}
